12695e356e47eba39d90383c74a19637fafc1dce
arafathemal <arafatfaiyaz@gmail.com>
2021-09-27 15:30:12 +0600

    data-fourth-commit

--- src/header/Header.js
+++ src/header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="header-style">
             <h2>List of schools And College in Bangladesh </h2>
             <h5>
-            <small>School is the basic foundation of knowledge being imparted to a child.</small>
+            <small>School is the basic foundation of knowledge being imparted to every child.</small>
             </h5>
             <h4>Total School: 10 Million +</h4>
             </div>
